d0d2ded407dfd2d44f884fda2b7f0a8611793164
Electrolion <AMccollum71@gmail.com>
2021-07-22 19:14:55 -0400

    HTML updated and JavaScript started

--- script.js
+++ script.js
@@ -2,5 +2,15 @@ const today = moment();
 const dateText = document.querySelector('#currentDay');
 dateText.innerHTML = today.format('MMMM Do YYYY');
 
-var tasks = {};
-
+const hour = document.querySelector('.hour');
+const textArea = document.querySelector('.textarea')
+const past = document.querySelector('.past')
+const present = document.querySelector('.present')
+const future = document.querySelector('.future')
+if (hour < today){
+    textArea.innerHTML = past;
+}else if (hour === today){
+    textArea.innerHTML = present;
+}else{
+    textArea.innerHTML = future;
+}

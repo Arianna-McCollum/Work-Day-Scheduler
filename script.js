1693ab9f562e2995abf6bcbfd487c51a177e0363
Electrolion <AMccollum71@gmail.com>
2021-07-23 12:24:27 -0400

    Hour ids changed to succesfully change classes

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 const dateText = document.querySelector('#currentDay');
 dateText.innerHTML = moment().format('MMMM Do YYYY');
 
-
-console.log($(".saveBtn"));
 $(".saveBtn").each(function(){
 $(this).on("click",function(){
     var time = $(this).parent().attr("id");
@@ -13,12 +11,12 @@ $(this).on("click",function(){
 })
 })
 
-timeNow=()=>{
+function timeNow(){
     var presentTime = moment().hour();
 
 
 $(".time-block").each(function(){
-    var rowTime = parseInt($(this).attr("id").split("hour")[1]);
+    var rowTime = parseInt($(this).attr("id").split("hour")[0]);
 
     if (rowTime > presentTime){
         $(this).removeClass("past");
@@ -28,7 +26,7 @@ $(".time-block").each(function(){
         $(this).removeClass("past");
         $(this).addClass("present");
         $(this).removeClass("future");
-    }else{
+    }else {
         $(this).addClass("past");
         $(this).removeClass("present");
         $(this).removeClass("future");
@@ -41,10 +39,10 @@ $("#9h .description").val(localStorage.getItem("9h"));
 $("#10h .description").val(localStorage.getItem("10h"));
 $("#11h .description").val(localStorage.getItem("11h"));
 $("#12h .description").val(localStorage.getItem("12h"));
-$("#1h .description").val(localStorage.getItem("1h"));
-$("#2h .description").val(localStorage.getItem("2h"));
-$("#3h .description").val(localStorage.getItem("3h"));
-$("#4h .description").val(localStorage.getItem("4h"));
-$("#5h .description").val(localStorage.getItem("5h"));
+$("#13h .description").val(localStorage.getItem("13h"));
+$("#14h .description").val(localStorage.getItem("14h"));
+$("#15h .description").val(localStorage.getItem("15h"));
+$("#16h .description").val(localStorage.getItem("16h"));
+$("#17h .description").val(localStorage.getItem("17h"));
 
 timeNow();
